648ad8ee0eae60c4b079cdf570eec4562abd3d36
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-20 20:55:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -15,4 +15,5 @@ def square_array_with_collect(array)
   array.collect {|el| el ** 2}
 end
 
-square_array_with_collect([2, 4, 6, 7])
+puts square_array([2, 4, 6, 7])
+puts square_array_with_collect([2, 4, 6, 7])
